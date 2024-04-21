ae1189be1b41d2651d5ca1e55f1a250d0480f07e
MICHELL <tmichell@uninorte.edu.co>
2024-04-20 20:22:09 -0500

    primer commit

---
+++ src/prueba2/Prueba2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba2;
+
+/**
+ *
+ * @author DELL-7040M
+ */
+public class Prueba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre="Fernando";
+ 
+        System.out.println("Bienvenido "+nombre);
+    }
+    
+}
